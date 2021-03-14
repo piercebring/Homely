d511266886812fab6928bb2c9bf1c20a53e19a8c
piercebring <35847326+piercebring@users.noreply.github.com>
2021-03-14 09:23:05 -0600

    Update environment.prod.ts

--- Homely/ClientApp/src/environments/environment.prod.ts
+++ Homely/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  stsAuthority: 'https://homelyidentityprovider.azurewebsites.net/',
+  stsAuthority: 'https://homelyidentityprovider.azurewebsites.net',
   clientRoot:'https://teamhomely.azurewebsites.net/',
   clientId: 'web-client',
   apiRoot: 'https://teamhomely.azurewebsites.net/',
